722afd9d5ad951d0d15208bede4b7b1af5060660
Мулюков Али <mulukovali@Mulukovs-MacBook-Air.local>
2020-12-01 01:43:05 +0600

    soccerbaseAV

--- tk_soccer1/soccerbaseAV.py
+++ tk_soccer1/soccerbaseAV.py
@@ -16,8 +16,17 @@ def learnMoreSeriaA(*args):
 
 def search(*args):
 	print("search")
+	# step 1 x
 	x = searchBar.get()
 	print(x)
+	# step 2 clear the entry widget
+	searchBar.delete(0, tk.END)
+	# step 3 change the state of textbox from disabled to normal
+	display.config(state = "normal")
+	# step 4 add x ot the textbox
+	display.insert(0.0, x)
+	# step 5 disable the texbox again
+	display.config(state = tk.DISABLED) 
 
 def currentteamspositions(*args):
 
@@ -37,13 +46,14 @@ btn2 = tk.Button(root,text = "Current team's positions",command = currentteamspo
 
 display = tk.Text(root,width = 30, height = 10)
 
-display.config(state = tk.DISABLED)
+display.config(state = tk.DISABLED) #manipulate
 
 labelSearchBar.grid(row = 0, column = 0, columnspan = 2)
 searchBar.grid(row = 1, column = 0, columnspan = 2)
 display1.grid(row = 2, column = 0, columnspan = 2)
 btn1.grid(row = 3, column = 0, columnspan = 2)
 btn2.grid(row =4, column = 0, columnspan = 2)
+display.grid(row =5, column = 0, columnspan = 2)
 
 
 root.bind("<Return>",search)
